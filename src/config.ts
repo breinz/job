81c280e089fc764b99748ec9ae10392c87bb8545
Julien Breiner <julien.breiner@gmail.com>
2019-01-20 21:31:56 +0100

    test

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ export default {
     port: env.PORT || 8100,
     mongoUri: "mongodb://breiner:haZkdh!7fa$61P00nS.N@mongodb-breiner.alwaysdata.net/breiner_site"
 
+
     /*port: env.PORT || 3000,
     mongoUri: "mongodb://0.0.0.0:27017/games"*/
 };
