1c12e4bbf2d8a0379eb7f849ffcff42e5afab9e3
Julien Breiner <julien.breiner@gmail.com>
2019-01-20 21:29:03 +0100

    test

--- dist/config.js
+++ dist/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let env = process.env;
 exports.default = {
-    port: env.PORT || 3000,
-    mongoUri: "mongodb://0.0.0.0:27017/games"
+    port: env.PORT || 8100,
+    mongoUri: "mongodb://breiner:haZkdh!7fa$61P00nS.N@mongodb-breiner.alwaysdata.net/breiner_site"
 };
 //# sourceMappingURL=config.js.map
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 let env = process.env;
 
 export default {
-    port: env.PORT || 3000,
-    mongoUri: "mongodb://0.0.0.0:27017/games"
+    port: env.PORT || 8100,
+    mongoUri: "mongodb://breiner:haZkdh!7fa$61P00nS.N@mongodb-breiner.alwaysdata.net/breiner_site"
+
+    /*port: env.PORT || 3000,
+    mongoUri: "mongodb://0.0.0.0:27017/games"*/
 };
