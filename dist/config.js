b338183514c7a6ad750f1c96969bbac51bfa7371
Julien Breiner <julien.breiner@gmail.com>
2019-01-20 22:13:30 +0100

    test

--- dist/config.js
+++ dist/config.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let env = process.env;
 exports.default = {
     port: env.PORT || 8100,
-    mongoUri: "mongodb://breiner:muP3htdqo@mongodb-breiner.alwaysdata.net/breiner_site"
+    mongoUri: "mongodb://breiner:muP3htdqo@mongodb-breiner.alwaysdata.net:27017/breiner_site"
 };
 //# sourceMappingURL=config.js.map
--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ let env = process.env;
 
 export default {
     port: env.PORT || 8100,
-    mongoUri: "mongodb://breiner:muP3htdqo@mongodb-breiner.alwaysdata.net/breiner_site"
+    mongoUri: "mongodb://breiner:muP3htdqo@mongodb-breiner.alwaysdata.net:27017/breiner_site"
     //"mongodb://breiner:haZkdh!7fa$61P00nS.N@mongodb-breiner.alwaysdata.net/breiner_site"
 
 
