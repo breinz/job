8c745c6fcc3d075a81451627586c5290b1b2e176
Julien Breiner <julien.breiner@gmail.com>
2019-01-21 19:05:58 +0100

    test

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,7 @@ const config_1 = require("./config");
 const app_1 = require("./app");
 let server = require("http").createServer(app_1.default);
 server.listen(config_1.default.port, "0.0.0.0", () => {
+    console.log(server.address());
     console.log(`Listening on ${config_1.default.port}`);
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import config from './config';
 import app from './app';
+import { Server } from 'http';
 //import socket from "./back/io";
 
 let server = require("http").createServer(app);
 
 server.listen(config.port, "0.0.0.0", () => {
+    console.log(server.address());
     console.log(`Listening on ${config.port}`)
 });
