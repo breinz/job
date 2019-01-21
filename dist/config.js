c2ae7271b949533c397422ad4178d8aba92cf309
Julien Breiner <julien.breiner@gmail.com>
2019-01-21 20:57:11 +0100

    test

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let env = process.env;
-exports.default = {
-    port: env.PORT || 8100,
-    mongoUri: "mongodb://breiner:muP3htdqo@mongodb-breiner.alwaysdata.net:27017/breiner_site"
-};
-//# sourceMappingURL=config.js.map
